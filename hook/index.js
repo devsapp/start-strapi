7663174964de9559fe3dbda61d519980f069faa0
yuankun.yk <yuankun.yk@alibaba-inc.com>
2022-03-06 18:00:09 +0800

    feat: hook钩子文件

--- hook/index.js
+++ hook/index.js
@@ -2,13 +2,13 @@ const http = require('http');
 const fs = require('fs');
 
 async function preInit(inputObj) {
-    console.log(`\n  _   _                
-    | | | | _____  _____  
-    | |_| |/ _ \\ \\/ / _ \\ 
-    |  _  |  __/>  < (_) |
-    |_| |_|\\___/_/\\_\\___/ 
-        
-                                         `)
+    console.log(`\n  ____  _                   _ 
+ / ___|| |_ _ __ __ _ _ __ (_)
+ \\___ \\| __| '__/ _\` | '_ \\| |
+  ___) | |_| | | (_| | |_) | |
+ |____/ \\__|_|  \\__,_| .__/|_|
+                     |_|   
+                                      `)
 }
 
 async function postInit(inputObj) {
